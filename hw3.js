949a1a9d4b06e71ac862404609b46c60118177a4
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-09-16 22:44:09 +0300

    all done

--- hw3.js
+++ hw3.js
@@ -46,3 +46,134 @@ for (let i = 0; i < users.length; i++) {
 //замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 
 
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+for (const item of listOfItems) {
+document.write(`<ul>
+                    <li>${item}</li>
+
+                </ul>`)
+
+}
+
+
+
+//Використовуючи данні з масиву, за допомоги document.write та циклу
+//побудувати структуру по шаблону
+//Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+//
+//let products = [
+//    {
+//        title: 'milk',
+//        price: 22,
+//        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//    },
+//    {
+//        title: 'juice',
+//        price: 27,
+//        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//    },
+//    {
+//        title: 'tomato',
+//        price: 47,
+//        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//    },
+//    {
+//        title: 'tea',
+//        price: 15,
+//        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//    },
+//];
+//
+//ШАБЛОН
+//<div class="product-card">
+//    <h3 class="product-title">TITLE. Price - PRICE</h3>
+//<img src="IMAGE" alt="" class="product-image">
+//</div>
+//Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+
+
+let products = [
+  {
+      title: 'milk',
+      price: 22,
+      image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+  },
+  {
+      title: 'juice',
+      price: 27,
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+  },
+  {
+      title: 'tomato',
+      price: 47,
+      image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+  },
+  {
+      title: 'tea',
+      price: 15,
+      image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+  },
+]
+
+for (let i = 0; i < products.length; i++) {
+
+    document.write(`<div class ="product-card">
+                       const product = products[i];
+                        <h3> ${products.title}- ${products.price}</h3>
+                        <img src="${products.image}" alt="products.image">
+                    </div>`)
+}
+
+//є масив
+//let users = [
+//    {name: 'vasya', age: 31, status: false},
+//    {name: 'petya', age: 30, status: true},
+//    {name: 'kolya', age: 29, status: true},
+//    {name: 'olya', age: 28, status: false},
+//    {name: 'max', age: 30, status: true},
+//    {name: 'anya', age: 31, status: false},
+//    {name: 'oleg', age: 28, status: false},
+//    {name: 'andrey', age: 29, status: true},
+//    {name: 'masha', age: 30, status: true},
+//    {name: 'olya', age: 31, status: false},
+//    {name: 'max', age: 31, status: true}
+//];
+//за допомоги циклу вивести:
+//    - користувачів зі статусом true
+//- користувачів зі статусом false
+//- користувачів які старші за 30 років
+
+let userses = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+for (const users1 of userses) {
+        if (users1.status === true) {
+            console.log(users1);
+        }
+else {
+    console.log(users1.name);
+        }
+
+}
+
+
+for (const users2 of userses) {
+    if (users2.age < 30) {
+        console.log(`${users2.name} - ${users2.age}`);
+    }
+    else {
+        console.log(`${users2.name}-FALSE`);
+    }
+}
