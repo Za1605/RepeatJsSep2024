4c7278e96f976b687b65cf1060eb755be675db52
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-10-09 23:17:40 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ HomeWork/hw6/hw6.js
@@ -0,0 +1,106 @@
+
+
+
+//
+//- Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//    let str = 'Ревуть воли як ясла повні';
+//let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+//
+//- є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+//- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+//    let nums = [11,21,3];
+//sortNums(nums,'ascending') // [3,11,21]
+//sortNums(nums,'descending') // [21,11,3]
+//
+//==========================
+//- є масив
+//let coursesAndDurationArray = [
+//    {title: 'JavaScript Complex', monthDuration: 5},
+//    {title: 'Java Complex', monthDuration: 6},
+//    {title: 'Python Complex', monthDuration: 6},
+//    {title: 'QA Complex', monthDuration: 4},
+//    {title: 'FullStack', monthDuration: 7},
+//    {title: 'Frontend', monthDuration: 4}
+//];
+//-- відсортувати його за спаданням за monthDuration
+//-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+//-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+//=========================
+//    описати колоду карт (від 6 до туза без джокерів)
+//- знайти піковий туз
+//- всі шістки
+//- всі червоні карти
+//- всі буби
+//- всі трефи від 9 та більше
+//
+//{
+//    cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//        value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+//    color:'', // 'red','black'
+//}
+//
+//=========================
+//
+//    Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+//{
+//    spades:[],
+//        diamonds:[],
+//    hearts:[],
+//    clubs:[]
+//}
+//=========================
+//    взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+//--написати пошук всіх об'єктів, в який в modules є sass
+//--написати пошук всіх об'єктів, в який в modules є docker
+
+
+//- Знайти та вивести довижину настипних стрінгових значень
+//'hello world', 'lorem ipsum', 'javascript is cool'
+
+const string = 'hello world';
+const string1 = 'lorem ipsum';
+const string2 = 'javascript is cool';
+console.log(string.length);
+console.log(string1.length);
+console.log(string2.length);
+
+
+//- Перевести до великого регістру наступні стрінгові значення
+//'hello world', 'lorem ipsum', 'javascript is cool'
+
+const upCa = 'hello world ,lorem ipsum ,javascript is cool';
+console.log(upCa.toUpperCase());
+
+
+//- Перевести до нижнього регістру настипні стрінгові значення
+//'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+const lowCase = 'HELLO WORLD, LOREM IPSUM, JAVASCRIPT IS COOL';
+console.log(lowCase.toLowerCase());
+
+//- Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+//function print(n) {
+//    console.log(n);
+//    if (n > 0) {
+//        print(n - 1);
+//    }
+//
+//}
+//print(5);
+
+let string111 = ' dirty string   ';
+let clStr = string111.replaceAll(' ', ' ');
+console.log(clStr);
+
+//- Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//    let str = 'Ревуть воли як ясла повні';
+//let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+
+function stringToarray(str) {
+    return.str.split('');
+}
+
+let strttttt = 'Ревуть воли як ясла повні';
+let arr11 = stringToarray(strttttt);
+console.log(arr11);
