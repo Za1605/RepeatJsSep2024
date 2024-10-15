553b0575c3493d9c41019791d957473254310f4c
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-10-15 23:29:01 +0300

    half1

--- HomeWork/hw6/hw6.js
+++ HomeWork/hw6/hw6.js
@@ -1,23 +1,5 @@
 
 
-//- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-//    let nums = [11,21,3];
-//sortNums(nums,'ascending') // [3,11,21]
-//sortNums(nums,'descending') // [21,11,3]
-//
-//==========================
-//- є масив
-//let coursesAndDurationArray = [
-//    {title: 'JavaScript Complex', monthDuration: 5},
-//    {title: 'Java Complex', monthDuration: 6},
-//    {title: 'Python Complex', monthDuration: 6},
-//    {title: 'QA Complex', monthDuration: 4},
-//    {title: 'FullStack', monthDuration: 7},
-//    {title: 'Frontend', monthDuration: 4}
-//];
-//-- відсортувати його за спаданням за monthDuration
-//-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-//-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 //=========================
 //    описати колоду карт (від 6 до туза без джокерів)
 //- знайти піковий туз
@@ -119,8 +101,44 @@ console.log(toString);
 
 
 let nums = [11,21,3];
-function sortNum(direction) {
-    return direction.a - direction.b
+function sortNum(arr,direction) {
+    if(direction === 'asc') {
+        return arr.sort((a,b) => a - b);
+    }
+    else if(direction === 'desc') {
+        return arr.sort((a,b) => b - a);
+    }
+    else {
+        return 'Error';
+    }
 }
 
-console.log(sortNum(nums,'ascending'));
+console.log(sortNum(nums,'asc'));
+console.log(sortNum(nums,'desc'));
+
+//- є масив
+//let coursesAndDurationArray = [
+//    {title: 'JavaScript Complex', monthDuration: 5},
+//    {title: 'Java Complex', monthDuration: 6},
+//    {title: 'Python Complex', monthDuration: 6},
+//    {title: 'QA Complex', monthDuration: 4},
+//    {title: 'FullStack', monthDuration: 7},
+//    {title: 'Frontend', monthDuration: 4}
+//];
+//-- відсортувати його за спаданням за monthDuration
+//-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+//-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+
+let coursesAndDurationArray = [
+ {title: 'JavaScript Complex', monthDuration: 5},
+ {title: 'Java Complex', monthDuration: 6},
+ {title: 'Python Complex', monthDuration: 6},
+ {title: 'QA Complex', monthDuration: 4},
+ {title: 'FullStack', monthDuration: 7},
+ {title: 'Frontend', monthDuration: 4}
+];
+
+console.log(coursesAndDurationArray.sort((a,b) => a.monthDuration - b.monthDuration));
+
+
+
